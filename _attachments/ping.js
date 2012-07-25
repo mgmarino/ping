56adca9b1ed377d70b57070979a2bc89bcbae117
Michael Marino <mmarino@gmail.com>
2012-07-25 12:20:55 +0200

    Adding replaceButtonText

--- _attachments/ping.js
+++ _attachments/ping.js
@@ -11,6 +11,29 @@ function sendping(){
 	}});
 }
 
+function replaceButtonText(buttonId, text)
+{
+  if (document.getElementById)
+  {
+    var button=document.getElementById(buttonId);
+    if (button)
+    {
+      if (button.childNodes[0])
+      {
+        button.childNodes[0].nodeValue=text;
+      }
+      else if (button.value)
+      {
+        button.value=text;
+      }
+      else //if (button.innerHTML)
+      {
+        button.innerHTML=text;
+      }
+    }
+  }
+}
+
 function checkdocs(){
 	$("#pongs").empty();
 	
